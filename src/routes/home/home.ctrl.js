47a0f0c5eaf507a577e2278f0c2b76fdc5642359
hskim0408 <hskim0408@medicalip.com>
2021-09-17 12:18:57 +0900

    UserStorage 모델 구현

--- src/routes/home/home.ctrl.js
+++ src/routes/home/home.ctrl.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const UserStorage = require("../../models/UserStorage");
 
 const output = {
     home : (req, res) =>{
@@ -11,30 +12,25 @@ const output = {
     },
 };
 
-const users = {
-    id: ["hs","나개발","김팀장"],
-    psword:["1234","1234","123456"],
-};
-
-
 const process = {
     login: (req, res) => {
        
         const id = req.body.id,
         psword = req.body.psword;
 
+        const users = UserStorage.getUsers('id','psword');
+
+        const response = {};
         if(users.id.includes(id)){
             const idx = users.id.indexOf(id);
             if(users.psword[idx]===psword){
-                return res.json({
-                    success:true,
-                });
+                response.success = true;
+                return res.json(response);
             }
         }
-        return res.json({
-            success:false,
-            msg:"로그인 실패"
-        });
+        response.success = false;
+        response.msg ="로그인 실패"
+        return res.json(response);
     },
 };
 
